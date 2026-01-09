2fbf8b2b02f251db5aab5ed9e550c42529efd64b
loopbutinfite <bestrada8800@gmail.com>
2026-01-09 15:59:12 -0800

    Added my peer review to the readme

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Mini Challenge Five to Seveen
 
 I Designed 4 controllers with designated services inside an API. the first controller was an ad lib story with user inputs, the second controller told someone if a number is odd or even, The third controller could take a number and reverse it, and the last controller did the same with both numbers and letters, including validation.
 
-Peer Review:
+Peer Review (Brandon Estrada): Your project is functional, I am able to retrieve the endpoints from your api along with my inputted values. Nice Job!
--- myapi/obj/Debug/net9.0/myapi.AssemblyInfo.cs
+++ myapi/obj/Debug/net9.0/myapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61a0a7a3143fb5189d296b36ee6fb4f4db66ef4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("myapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
